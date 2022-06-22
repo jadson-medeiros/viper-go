e66af50ce88d055cb3e0fe2682f54f682c5d6883
Jadson Medeiros <developer.medeiros@gmail.com>
2022-06-22 19:40:05 -0300

    testing viper working with environment variables

--- main.go
+++ main.go
@@ -2,8 +2,23 @@ package main
 
 import (
 	"fmt"
+	"os"
+
+	"github.com/spf13/viper"
 )
 
 func main() {
-	fmt.Println("Stating to test viper...")
+	// This is usually set outside the program
+	os.Setenv("PREFIX_VAR", "hello")
+	os.Setenv("PREFIX_VAR2", "hello2")
+
+	viper.SetEnvPrefix("prefix")
+	viper.AutomaticEnv()
+
+	fmt.Println(viper.Get("var"))
+	fmt.Println(viper.Get("var2"))
+
+	if !viper.IsSet("home") {
+		fmt.Println("The key 'home' is not set")
+	}
 }
